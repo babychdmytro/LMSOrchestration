39e85088c752cdbc912b4250036dc430aa3b21aa
DESKTOP-DMYTRO\babyc <babych.dmt@gmail.com>
2018-04-18 17:28:00 -0400

    d

--- src/main/java/com/gcit/lms/LmsOrchestrationApplication.java
+++ src/main/java/com/gcit/lms/LmsOrchestrationApplication.java
@@ -11,7 +11,7 @@ public class LmsOrchestrationApplication {
 	//test jenkins14
 	public static void main(String[] args) {
 		SpringApplication.run(LmsOrchestrationApplication.class, args);
-		System.out.println("deplsdssssoysdaaaas");
+		System.out.println("deplsdsssssoysdaaaas");
 	}
 	
 	@Bean
