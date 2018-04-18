bbce9396d13f98a41461ba5fd35ef5680aca3048
DESKTOP-DMYTRO\babyc <babych.dmt@gmail.com>
2018-04-18 11:13:57 -0400

    12

--- src/main/java/com/gcit/lms/LmsOrchestrationApplication.java
+++ src/main/java/com/gcit/lms/LmsOrchestrationApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class LmsOrchestrationApplication {
-	//test jenkins11
+	//test jenkins12
 	public static void main(String[] args) {
 		SpringApplication.run(LmsOrchestrationApplication.class, args);
 	}
