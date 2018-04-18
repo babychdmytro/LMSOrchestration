900edf7c7df56c3ec95286c3398772e4fc3fcfcb
babychdmytro <babych.dmt@gmail.com>
2018-04-18 01:12:57 -0400

    Update LmsOrchestrationApplication.java

--- src/main/java/com/gcit/lms/LmsOrchestrationApplication.java
+++ src/main/java/com/gcit/lms/LmsOrchestrationApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class LmsOrchestrationApplication {
-
+	//test jenkins
 	public static void main(String[] args) {
 		SpringApplication.run(LmsOrchestrationApplication.class, args);
 	}
