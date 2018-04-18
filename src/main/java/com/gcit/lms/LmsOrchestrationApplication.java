248c8e1dda05d91a854259bedfa70d453ce5ca1d
DESKTOP-DMYTRO\babyc <babych.dmt@gmail.com>
2018-04-18 16:14:48 -0400

    deploy

--- src/main/java/com/gcit/lms/LmsOrchestrationApplication.java
+++ src/main/java/com/gcit/lms/LmsOrchestrationApplication.java
@@ -11,7 +11,7 @@ public class LmsOrchestrationApplication {
 	//test jenkins14
 	public static void main(String[] args) {
 		SpringApplication.run(LmsOrchestrationApplication.class, args);
-		System.out.println("test s322");
+		System.out.println("deploy");
 	}
 	
 	@Bean
